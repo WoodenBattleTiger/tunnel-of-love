098f95870334d2019d5755bbb79bbbc8dcdbbddf
cmaguiar <aguiar.claire@gmail.com>
2025-09-26 14:48:20 -0400

    score & combo

--- scripts/arrow.gd
+++ scripts/arrow.gd
@@ -3,6 +3,9 @@ extends Node2D
 var dir = 1
 @export var speed = 10
 
+signal update_combo
+signal clear_combo
+
 func _physics_process(_delta: float) -> void:
 	
 	position.x += speed * dir
@@ -12,3 +15,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Monster:
 		body.queue_free()
 		queue_free()
+		if body.type == body.Type.MONSTER:
+			update_combo.emit(1)
+		else:
+			clear_combo.emit()
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
+@export var monster_patience_penalty = 15
+
+@export var distraction_patience_bonus = 15
+@export var distraction_collect_bonus = 2
+
 @onready var r: RhythmNotifier = $RhythmNotifier 
 
 var patience = 100.0
 var patience_timer: SceneTreeTimer
+
+var score = 0
+var combo = 1
 
 func _ready() -> void:
 	patience_timer = get_tree().create_timer(1)
@@ -29,7 +37,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			$LeftArea.monitoring = !$LeftArea.monitoring
 			$LeftArea/CollisionShape2D/ColorRect.visible = !$LeftArea/CollisionShape2D/ColorRect.visible
 			$RightArea/CollisionShape2D/ColorRect.visible = !$RightArea/CollisionShape2D/ColorRect.visible
-	if event.is_action_pressed("shoot"):
+	if event.is_action_pressed("shoot") && $ShootTimer.is_stopped():
 		if get_viewport().get_mouse_position().x > get_viewport_rect().size.x / 2:
 			spawn_arrow(1)
 			print("shoot right")
@@ -42,17 +50,21 @@ func spawn_arrow(dir):
 	new_arrow.dir = dir
 	new_arrow.position.x = get_viewport_rect().size.x / 2
 	new_arrow.position.y = get_viewport_rect().size.y / 2
+	new_arrow.connect("update_combo", update_combo)
+	new_arrow.connect("clear_combo", clear_combo)
 	add_child(new_arrow)
+	$ShootTimer.start()
 	
 func body_entered(body:Node2D):
 	print(body.name)
 	if body is Monster:
 		if !body.collected:
 			if body.type == body.Type.MONSTER:
-				update_patience(-15)
+				update_patience(-1 * monster_patience_penalty)
 				body.collected = true
 			else:
-				update_patience(15)
+				update_patience(distraction_patience_bonus)
+				update_score(distraction_collect_bonus)
 				body.collected = true
 	
 func update_patience(val):
@@ -65,4 +77,16 @@ func update_patience(val):
 		print("game over!")
 	
 	$Patience.value = patience
+	
+func update_combo(val):
+	combo += val
+	$Combo.text = "Combo: x" + str(combo)
+	
+func clear_combo():
+	combo = 1
+	$Combo.text = "Combo: x" + str(combo)
+	
+func update_score(val):
+	score += val * combo
+	$Score.text = "Score: " + str(score)
 	
