b615f6981139d1cdd674ac361c8aafc59b4cb916
cmaguiar <aguiar.claire@gmail.com>
2025-09-27 22:24:08 -0400

    modified spawn rates

--- scripts/monster_manager.gd
+++ scripts/monster_manager.gd
@@ -8,19 +8,18 @@ var right_spawn: Vector2
 func spawn_monster():
 	var new_monster = monster_scene.instantiate()
 	var spawn_side = randi_range(1,2)
-	var type = randi_range(1,2)
+	var type = randf_range(0.0,1.0)
 	
 	left_spawn = Vector2((get_viewport_rect().size.x / 2) - 100, 200)
 	right_spawn = Vector2( (get_viewport_rect().size.x / 2) + 100, 200)
+		
 	
-	if type == 2:
-		left_spawn.y -= 100
-		right_spawn.y -= 100
-	
-	if type == 1:
+	if type < 0.75:
 		new_monster.type = new_monster.Type.MONSTER
 	else:
 		new_monster.type = new_monster.Type.ATTRACTION
+		left_spawn.y -= 100
+		right_spawn.y -= 100
 	new_monster.set_sprite()
 	if spawn_side == 1:
 		new_monster.position = left_spawn
